faafc53bdceaed06b121ae090edbd06c1b8ad01e
jonathanlukass <jhonatanlukas05@gmail.com>
2024-03-01 16:15:37 +0800

    moneytracker

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ['module-resolver', {
+      root: ['./'],
+      extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.svg'],
+      alias: {
+        '@components': './src/components',
+        '@assets': './src/assets',
+        '@screens': './src/screens',
+        '@navigation': './src/navigation',
+        '@services': './src/services',
+        '@utils': './src/utils',
+      },
+    }],
+    'react-native-svg',
+  ],
 };
